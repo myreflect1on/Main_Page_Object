1ada302b4972f25a4c55a727f71a5b975114ab7e
myreflect1on <z.denis.mail@gmail.com>
2021-12-17 08:47:38 +0500

    Initial commit

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
 import time
+from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    time.sleep(5)
-    login_link.click()
