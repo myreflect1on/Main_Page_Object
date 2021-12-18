978716ffe63431e6a79afc017dafdfa70b6efef9
myreflect1on <z.denis.mail@gmail.com>
2021-12-18 14:21:03 +0500

    Initial commit

--- test_product_page.py
+++ test_product_page.py
@@ -47,13 +47,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -90,3 +91,12 @@ class TestUserAddToBasketFromProductPage():
         page.find_button_basket()
         page.should_be_message_about_adding()
         page.should_be_message_basket_total()
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.find_button_basket()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
